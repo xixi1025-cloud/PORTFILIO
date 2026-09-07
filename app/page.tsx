4a4c89ab7301ff29a33c1b92d465971129585865
xixi1025-cloud <3443024652@qq.com>
2026-09-07 20:16:02 +0800

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -8,5 +8,5 @@ import { Navbar } from '../components/Navbar';
 import { Portfolio } from '../components/Portfolio';
 
 export default function Home() {
-  return <><Navbar /><main><Hero /><About /><Experience /><Portfolio /><Awards /><Contact /></main><Footer /></>;
+  return <><Navbar home /><main><Hero /><About /><Experience /><Portfolio /><Awards /><Contact /></main><Footer /></>;
 }
